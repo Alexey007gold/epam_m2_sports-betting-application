76f8ddfae6dd0f3c9c3af11d604b3e748246aa98
Oleksii_Kovetskyi <Oleksii_Kovetskyi@epam.com>
2018-12-10 14:29:33 +0200

    Set up Spring webmvc

---
+++ sports-betting-application-app/src/main/java/com/epam/training/sportsbetting/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.epam.training.sportsbetting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableAspectJAutoProxy
+@EnableWebMvc
+@ComponentScan(basePackages = "com.epam.training.sportsbetting")
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/views/jsp/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
